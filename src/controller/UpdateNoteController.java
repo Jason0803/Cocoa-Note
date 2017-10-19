3e8f0f871c0be578364cf4d1eff333fa0e6092e9
Jason <c3171700@uon.edu.au>
2017-10-19 12:19:41 +0900

    #00080 : 5 New Controller.java files added
    > Need implementation
    - NoteViewController
    - ScheduleViewController
    - DeleteDiaryController
    - UpdateNoteController
    - UpdateScheduleController

---
+++ src/controller/UpdateNoteController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.util.ModelAndView;
+
+public class UpdateNoteController implements Controller {
+
+	@Override
+	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
