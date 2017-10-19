ff046f510505531f76b1e16cad3e6731a6053594
Jason <c3171700@uon.edu.au>
2017-10-19 14:51:06 +0900

    #00082 : Frame Update
    - Fixed HandlerMapping typo
    - Controller Frame
    - DAO Frames

--- src/controller/UpdateScheduleController.java
+++ src/controller/UpdateScheduleController.java
@@ -1,16 +1,24 @@
 package controller;
 
+import java.util.Vector;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.util.ModelAndView;
+import dao.diary.DiaryDAO;
+import vo.diary.Memo;
+import vo.diary.Schedule;
+import vo.member.Member;
 
 public class UpdateScheduleController implements Controller {
 
 	@Override
 	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Member mvo = (Member) request.getSession().getAttribute("memberVO");
+		// Schedule schedule = DiaryDAO.getInstance().getAllMemo(mvo.getId());
+		request.setAttribute("schedule", schedule);
+		return new ModelAndView("update_schedule.jsp");
 	}
 
 }
